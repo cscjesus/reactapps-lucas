05f8d6961ebd9a2e3b8dbad786b203d1aac4cfa8
Jesus Hernandez <csc.jesus@gmail.com>
2022-08-06 09:55:28 -0500

    hora 1.48

--- peliculas/src/App.js
+++ peliculas/src/App.js
@@ -1,24 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import Pelicula from './Pelicula';
+import PageWrapper from './PageWrapper';
+import peliculasJson from './peliculas.json';
 
 function App() {
+  let peliculas = peliculasJson;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PageWrapper>
+      {
+        // peliculas.map(pelicula => {
+        //   return (
+        //     <Pelicula titulo={pelicula.titulo} calificacion={pelicula.calificacion}
+        //       director={pelicula.director} actores={pelicula.director} fecha={pelicula.fecha} duracion={pelicula.duracion} img={pelicula.img}>
+        //       Earth's mightiest heroes must come together and learn to fight as a team if they are to stop the mischievous Loki and his alien army from enslaving humanity...
+        //     </Pelicula>
+        //   );
+        // })
+        peliculas.map(pelicula =>
+          <Pelicula titulo={pelicula.titulo} calificacion={pelicula.calificacion}
+            director={pelicula.director} actores={pelicula.director} fecha={pelicula.fecha} duracion={pelicula.duracion} img={pelicula.img}>
+            {pelicula.descripcion}
+          </Pelicula>
+        )
+      }
+
+
+
+
+
+      {/* <Pelicula titulo="into the wild (2014)" calificacion="7.8"
+      director="Joss Whedon" actores="Robert Downey Jr., Chris Evans, Chris Hemsworth" fecha="1 May 2015" duracion="2h 21min" img="images/uploads/mv2.jpg">
+      Earth's mightiest heroes must come together and learn to fight as a team if they are to stop the mischievous Loki and his alien army from enslaving humanity...
+    </Pelicula> */}
+    </PageWrapper>
+
   );
 }
 
